4861d618bf8fc32eb194940d5cec3527ace98138
d5732 <denwa.dev@gmail.com>
2023-01-31 02:37:59 -0600

    feat: PATCH /users/{id}

--- group-final/src/main/java/com/cooksys/groupfinal/controllers/UserController.java
+++ group-final/src/main/java/com/cooksys/groupfinal/controllers/UserController.java
@@ -1,28 +1,28 @@
 package com.cooksys.groupfinal.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.cooksys.groupfinal.dtos.BasicUserDto;
 import com.cooksys.groupfinal.dtos.CredentialsDto;
 import com.cooksys.groupfinal.dtos.FullUserDto;
 import com.cooksys.groupfinal.services.UserService;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-	
-	private final UserService userService;
-	
-	@PostMapping("/login")
-	@CrossOrigin(origins="*")
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    @CrossOrigin(origins = "*")
     public FullUserDto login(@RequestBody CredentialsDto credentialsDto) {
         return userService.login(credentialsDto);
+    }
+
+    @PatchMapping("/{id}")
+    public BasicUserDto updateUser(@PathVariable Long id, @RequestBody BasicUserDto basicUserDto) {
+        return userService.updateUser(id, basicUserDto);
     }
 
 }
