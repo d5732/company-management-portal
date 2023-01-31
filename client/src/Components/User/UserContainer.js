d7f9d248068f8223965402abc93c46a6e9fa88a2
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-01-31 02:59:27 +0100

    Add: user info table on users page

--- client/src/Components/User/UserContainer.js
+++ client/src/Components/User/UserContainer.js
@@ -1,28 +1,75 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import mockData from '../../data.json'
+import userTableColumns from './userTableColumns'
+import { useTable } from 'react-table'
 
 
-// import { readFile } from 'fs/promises'
-// const json = JSON.parse(
-    //   await readFile(new URL('../../data.JSON', import.meta.url))
-    // )
-    
 const UserContainer = () => {
-    const [users, setUser] = useState(mockData.data.users)
+  const [users, setUser] = useState(mockData.data.users)
 
-    console.log(users)
+  const userTableData = users.map((user) => ({
+    name: user.profile.firstname + ' ' + user.profile.lastname,
+    email: user.profile.email,
+    phone: user.profile.phone,
+    team: user.teams[0].name,
+    active: user.active === true ? "YES" : "NO",
+    admin: user.admin === true ? "YES" : "NO",
+    status: user.status,
+  }))
+  
+  const columns = useMemo(() => userTableColumns, [])
+  const data = useMemo(() => [...userTableData, userTableData])
 
-        return (
-            <div>
-              <h1>User Registry</h1>
-              <h2>A general view of all members in your orginazation</h2>
-              <h3>{users[0].id}</h3>
-              
-              <button>ADD USER</button>
-              {/* <button onClick={() => console.log({card.data.users[0]})}>debug</button> */}
-          </div>
-        )
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    })
+
+  return (
+    <div>
+      <h1>User Registry</h1>
+      <h2>A general view of all members in your orginazation</h2>
+      <>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render('Header')}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {
+              rows.map((row, i) => {
+                prepareRow(row)
+                return (
+                  <tr {...row.getRowProps()}>
+                    {
+                      row.cells.map((cell) => {
+                        return (
+                          <td {...cell.getCellProps()}>
+                            {cell.render('Cell')}
+                          </td>
+                        )
+                      })
+                    }
+                  </tr>
+                )
+            })
+        }
+          </tbody>
+        </table>
+      </>
+
+      <button>ADD USER</button>
+    </div>
+  )
 }
 
 export default UserContainer
---
+++ client/src/Components/User/userTableColumns.js
@@ -0,0 +1,33 @@
+
+const userTableColumns = [
+    {
+    Header: "Name",
+    accessor: "name"
+    },
+    {
+    Header: "Email",
+    accessor: "email"
+    },
+    {
+    Header: "Phone",
+    accessor: "phone"
+    },
+    {
+    Header: "Team",
+    accessor: "team"
+    },
+    {
+    Header: "Active",
+    accessor: "active"
+    },
+    {
+    Header: "Admin",
+    accessor: "admin"
+    },
+    {
+    Header: "Status",
+    accessor: "status"
+    }
+]
+
+export default userTableColumns
