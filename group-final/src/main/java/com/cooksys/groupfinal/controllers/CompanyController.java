85cfd06e52bda93dc101bb2c72869e9decdce3ec
Martin Hu <martin.m.hu89@gmail.com>
2023-01-31 16:58:02 -0500

    added @CrossOrigin(origins = *) to all controller files

--- group-final/src/main/java/com/cooksys/groupfinal/controllers/CompanyController.java
+++ group-final/src/main/java/com/cooksys/groupfinal/controllers/CompanyController.java
@@ -16,36 +16,43 @@ public class CompanyController {
     private final CompanyService companyService;
 
     @GetMapping("/{id}/users")
+    @CrossOrigin(origins = "*")
     public Set<FullUserDto> getAllUsers(@PathVariable Long id) {
         return companyService.getAllUsers(id);
     }
     
     @GetMapping("/")
+    @CrossOrigin(origins = "*")
     public List<CompanyDto> getAllCompanies() {
     	return companyService.getAllCompanies();
     }
 	
 	@GetMapping("/{id}/users/active")
+	@CrossOrigin(origins = "*")
     public Set<BasicUserDto> getAllActiveUsers(@PathVariable Long id) {
         return companyService.getAllActiveUsers(id);
     }
 	
 	@GetMapping("/{id}/announcements")
+	@CrossOrigin(origins = "*")
     public Set<AnnouncementDto> getAllAnnouncements(@PathVariable Long id) {
         return companyService.getAllAnnouncements(id);
     }
 
     @GetMapping("/{id}/teams")
+    @CrossOrigin(origins = "*")
     public Set<TeamDto> getAllTeams(@PathVariable Long id) {
         return companyService.getAllTeams(id);
     }
 
     @PostMapping("/{id}/teams")
+    @CrossOrigin(origins = "*")
     public TeamDto createTeam(@PathVariable Long id, @RequestBody TeamDto teamDto) {
         return companyService.createTeam(id, teamDto);
     }
 
     @PostMapping("/{id}/users")
+    @CrossOrigin(origins = "*")
     public BasicUserDto createUser(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto) {
         return companyService.createUser(id, userRequestDto);
     }
@@ -53,6 +60,7 @@ public class CompanyController {
 	// createAnnouncement
 	
 	@PostMapping("/{id}/announcements")
+	@CrossOrigin(origins = "*")
 	public AnnouncementDto createAnnouncement(@PathVariable Long id, @RequestBody AnnouncementDto announcementDto) {
 		
 		return companyService.createAnnouncement(id, announcementDto);
@@ -62,6 +70,7 @@ public class CompanyController {
 	// updateProject
 	
 	@PatchMapping("/{id}/teams/{teamID}/projects")
+	@CrossOrigin(origins = "*")
 	public ProjectDto updateProject(@PathVariable Long id, @PathVariable Long teamID, @RequestBody ProjectDto projectDto) {
 	
 		return companyService.updateProject(id, teamID, projectDto);
